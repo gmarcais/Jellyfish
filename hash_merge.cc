ce053f2352dd86fee3cae4b4da1d4d910ea19f51
Carl Kingsford <carkl@umiacs.umd.edu>
2010-06-23 21:39:57 +0000

    added serial_writer
    git-svn-id: https://svn.cs.umd.edu/repos/carlksvn/jellyfish/src@383 3840e3e0-9109-4a78-8b6e-c2447a23df0a

--- hash_merge.cc
+++ hash_merge.cc
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <stdlib.h>
 #include <argp.h>
+#include <iostream>
+#include <fstream>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <stdint.h>
@@ -12,6 +14,7 @@
 #include <algorithm>
 
 #include "mer_counting.hpp"
+#include "compacted_hash.hpp"
 #include "lib/misc.hpp"
 
 
@@ -20,21 +23,21 @@
  * the hashes
  **/
 struct heap_item {
-   mer_counters::iterator *it;
-   uint64_t key;
-   uint64_t val;
-   uint64_t hash;
+  mer_counters::iterator *it;
+  uint64_t key;
+  uint64_t val;
+  uint64_t hash;
 
-   heap_item() : it(0), key(0), val(0), hash(0) {}
+  heap_item() : it(0), key(0), val(0), hash(0) {}
 
-   heap_item(mer_counters::iterator * iter) :
-     it(iter), key(iter->key), val(iter->val), hash(iter->get_hash()) {}
+  heap_item(mer_counters::iterator * iter) :
+    it(iter), key(iter->key), val(iter->val), hash(iter->get_hash()) {}
 
-   bool operator<(const heap_item & other) {
-      if(hash == other.hash)
-        return key < other.key;
-      return hash < other.hash;
-   }
+  bool operator<(const heap_item & other) {
+     if(hash == other.hash)
+       return key < other.key;
+     return hash < other.hash;
+  }
 };
 
 /**
@@ -45,13 +48,21 @@ const char *argp_program_bug_address = "<guillaume@marcais.net>";
 static char doc[] = "Merge dumped hash tables";
 static char args_doc[] = "";
 
+enum {
+   OPT_VAL_LEN = 1024
+};
+
 static struct argp_option options[] = {
   {"fasta",     'f',    0,  0,  "Print k-mers in fasta format (false)"},
+  {"output",    'o',  "FILE", 0, "Output file"},
+  {"out-counter-len", OPT_VAL_LEN, "LEN", 0, "Length (in bytes) of counting field in output"},
   {0}
 };
 
 struct arguments {
   bool fasta;
+  char * output;
+  unsigned int out_counter_len;
 };
 
 
@@ -68,9 +79,12 @@ if(errno) return errno; \
 break;
 
 #define FLAG(field) arguments->field = true; break;
+#define STRING(field) arguments->field = arg; break;
 
   switch(key) {
   case 'f': FLAG(fasta);
+  case 'o': STRING(output);
+  case OPT_VAL_LEN: ULONGP(out_counter_len);
 
   default:
     return ARGP_ERR_UNKNOWN;
@@ -94,6 +108,77 @@ inline void tostring(uint64_t key, unsigned int rklen, char * out) {
   out[rklen] = '\0';
 }
 
+class ErrorWriting : public std::exception {
+   std::string msg;
+  public:
+   ErrorWriting(const std::string _msg) : msg(_msg) {}
+   virtual ~ErrorWriting() throw() {}
+   virtual const char* what() const throw() {
+     return msg.c_str();
+   }
+};
+
+
+class serial_writer {
+   uint64_t mer_len_bases;
+   uint64_t val_len_bits;
+   uint64_t key_len; // in BYTES
+   uint64_t val_len; // in BYTES
+   uint64_t max_count;
+   std::ofstream out;
+   char * buffer;
+   
+   public:
+     // klen is in BASES; vlen is in BITs
+     serial_writer(std::string filename, uint_t klen, uint_t vlen) :
+        mer_len_bases(klen),
+        val_len_bits(vlen),
+        key_len((klen / 4) + (klen % 4 != 0)),
+        val_len((vlen / 8) + (vlen % 8 != 0)),
+        max_count((((uint64_t)1) << (8*val_len)) - 1)
+    {
+        out.open(filename.c_str(), std::ios::binary);
+        if(!out.good())
+           throw new ErrorWriting("Can't open file");
+        buffer = new char[key_len + val_len];
+    }
+
+    ~serial_writer() {
+        delete [] buffer;
+    }
+
+    uint64_t get_key_len_bytes() { return key_len; }
+    uint64_t get_val_len_bytes() { return val_len; }
+
+    /**
+     * Write the top of the hash table
+     **/
+    void write_header(uint64_t size) {
+        jellyfish::compacted_hash::header head;
+        memset(&head, '\0', sizeof(head));
+        head.mer_len = mer_len_bases;
+        head.val_len = val_len_bits;
+        head.size = size;
+        out.write((char *)&head, sizeof(head));
+    }
+
+    /**
+     * Write a single key, value entry
+     **/
+    void write_entry(uint64_t key, uint64_t val) {
+      char *ptr = buffer;
+      memcpy(ptr, &key, key_len);
+      ptr += key_len;
+      uint64_t count = (val > max_count) ? max_count : val;
+      memcpy(ptr, &count, val_len);
+      out.write(buffer, key_len + val_len);
+    }
+
+    void close() {
+        // XXX: nop 
+    }
+};
+
 
 int main(int argc, char *argv[]) {
 
@@ -101,11 +186,14 @@ int main(int argc, char *argv[]) {
   struct arguments cmdargs;
   int arg_st = 1;
   cmdargs.fasta = false;
+  cmdargs.out_counter_len = 4;
+  cmdargs.output = "mer_counts_merged.hash";
   argp_parse(&argp, argc, argv, 0, &arg_st, &cmdargs);
 
   int i;
   unsigned int rklen = 0;
   size_t max_reprobe = 0;
+  uint64_t size = 0;
 
   // compute the number of hashes we're going to read
   int num_hashes = argc - arg_st;
@@ -143,6 +231,9 @@ int main(int argc, char *argv[]) {
     if(max_reprobe != 0 && rep != max_reprobe)
        die("Can't merge hashes with different reprobing stratgies\n");
     max_reprobe = rep;
+
+    uint64_t si = tables[i-arg_st]->get_size();
+    fprintf(stderr, "size = %ld\n", si);
   }
 
   if(max_reprobe == 0 || rklen == 0)
@@ -171,12 +262,19 @@ int main(int argc, char *argv[]) {
   if(h == 0) 
     die("Hashes contain no items.");
 
+  // open the output file
+  serial_writer writer(cmdargs.output, rklen, cmdargs.out_counter_len);
+  writer.write_header(size);
+
+  fprintf(stderr, "out kmer len = %ld bytes\n", writer.get_key_len_bytes());
+  fprintf(stderr, "out val len = %ld bytes\n", writer.get_val_len_bytes());
+
   // order the heap
   make_heap(heap, heap + h);
 
   // h is the current heap size
   while(h > 0) {
-    char out[rklen+1];
+    //char out[rklen+1];
     int starth = h;
 
     // pop one element off
@@ -214,8 +312,9 @@ int main(int argc, char *argv[]) {
     }
 
     // for debugging, just write out the counts
-    tostring(key, rklen, out);
-    std::cout << out << " " << sum << endl; 
+    //tostring(key, rklen, out);
+    //std::cout << out << " " << sum << endl; 
+    writer.write_entry(key, sum);
   }
 
   // free the hashes and iterators
@@ -223,22 +322,6 @@ int main(int argc, char *argv[]) {
      delete iters[i];
      delete tables[i];
   }
+
+  writer.close();
 }
-/*
-    fd = open(db_file, O_RDONLY);
-    if(fd < 0)
-      die("Can't open '%s'\n", db_file);
-
-    if(fstat(fd, &finfo) < 0)
-      die("Can't stat '%s'\n", db_file);
-
-    map = (char *)mmap(NULL, finfo.st_size, PROT_READ, MAP_SHARED, fd, 0);
-    if(map == MAP_FAILED)
-      die("Can't mmap '%s'\n", db_file);
-    int adv = madvise(map, finfo.st_size, MADV_SEQUENTIAL);
-    if (adv != 0)
-      die("Can't set memory parameters correctly\n");
-    close(fd);
-
-    tables[i-arg_st] = new mer_counters(map, finfo.st_size);
-*/
