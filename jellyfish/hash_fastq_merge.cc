531788357e4b047fa468f1176f40804b38240cdf
Guillaume Marcais <gmarcais@umd.edu>
2011-03-30 15:12:30 +0000

    Merging of quake databases.
    git-svn-id: https://svn.cs.umd.edu/repos/carlksvn/jellyfish/branch-releases/stream-parsing@1036 3840e3e0-9109-4a78-8b6e-c2447a23df0a

---
+++ jellyfish/hash_fastq_merge.cc
@@ -0,0 +1,58 @@
+/*  This file is part of Jellyfish.
+
+    Jellyfish is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Jellyfish is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Jellyfish.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <hash_fastq_merge_cmdline.hpp>
+#include <jellyfish/err.hpp>
+#include <jellyfish/misc.hpp>
+#include <jellyfish/mer_counting.hpp>
+#include <jellyfish/fastq_dumper.hpp>
+
+int hash_fastq_merge_main(int argc, char *argv[])
+{
+  struct hash_fastq_merge_args args;
+
+  if(hash_fastq_merge_cmdline(argc, argv, &args) != 0)
+    die << "Command line parser failed";
+
+  if(args.inputs_num < 2)
+    die << "Need at least 2 quake databases\n"
+        << hash_fastq_merge_args_usage << "\n" << hash_fastq_merge_args_help;
+
+  fastq_hash_t::storage_t ary(args.size_arg, 2*args.mer_len_arg,
+                              args.reprobes_arg, jellyfish::quadratic_reprobes);
+  fastq_hash_t hash(&ary);
+  fastq_hash_t::thread_ptr_t counter(hash.new_thread());
+  
+  for(unsigned int i = 0; i < args.inputs_num; ++i) {
+    fastq_storage_t *ihash = raw_fastq_dumper_t::read(args.inputs[i]);
+    if(ihash->get_key_len() != hash.get_key_len())
+      die << "Invalid mer length '" << (ihash->get_key_len() / 2)
+          << "' for database '" << args.inputs[i] 
+          << "'. Should be '" << (hash.get_key_len() / 2) << "'";
+    fastq_storage_t::iterator iit = ihash->iterator_all();
+    while(iit.next()) {
+      counter->add(iit.get_key(), iit.get_val().to_float());
+    }
+    delete ihash;
+  }
+
+  raw_fastq_dumper_t dumper(args.threads_arg, args.output_arg, 
+                            args.out_buffer_size_arg, &ary);
+  hash.set_dumper(&dumper);
+  hash.dump();
+
+  return 0;
+}
---
+++ tests/merge.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+. ./compat.sh
+
+sort > ${pref}.md5sum <<EOF
+c448e173e5e18264ed00fad0008d5340 ${pref}_quake.histo
+EOF
+echo "Counting mers and merging" && \
+    $JF count --quake --matrix seq10m_matrix_19 -m 19 -t $nCPUs \
+    -o ${pref}_quake -s 3000000 --timing ${pref}_quake.timing seq10m.fq && \
+    $JF qmerge -s 10000000 -m 19 -o ${pref}_quake_merged ${pref}_quake_[012] && \
+    $JF qhisto -h 3 -i 0.01 -l 0 ${pref}_quake_merged_0 > ${pref}_quake.histo && \
+    check ${pref}.md5sum
+RET=$?
+
+echo "Qmer timing"; cat ${pref}_quake.timing 2>/dev/null
+exit $RET
