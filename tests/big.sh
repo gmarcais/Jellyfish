gmarcais/Jellyfish

commit dae3ed23b52b32e3df015dc5617cd5b62dd1eb7f

Fix bug: if hash array size is less than reprobe value, lower reprobe limit.
git-svn-id: https://svn.cs.umd.edu/repos/carlksvn/jellyfish/branch-releases/version-1.1@1340 3840e3e0-9109-4a78-8b6e-c2447a23df0a

jellyfish/invertible_hash_array.hpp
@@ -39,6 +39,22 @@ namespace jellyfish {
     define_error_class(ErrorAllocation);
     define_error_class(InvalidMatrix);
 
+    /* Contains an integer, the reprobe limit. It is capped based on
+     * the reprobe strategy to not be bigger than the size of the hash
+     * array.
+     */
+    class reprobe_limit_t {
+      uint_t limit;
+    public:
+      reprobe_limit_t(uint_t _limit, size_t *_reprobes, size_t _size) :
+        limit(_limit)
+      {
+        while(_reprobes[limit] >= _size && limit >= 1)
+          limit--;
+      }
+      inline uint_t val() const { return limit; }
+    };
+
     /* (key,value) pair bit-packed array.  It implements the logic of the
      * packed hash except for size doubling. Setting or incrementing a key
      * will return false if the hash is full. No memory management is done
@@ -58,8 +74,7 @@ namespace jellyfish {
       typedef typename Offsets<word>::offset_t offset_t;
       uint_t             lsize;    // log of size
       size_t             size, size_mask;
-      uint_t             reprobe_limit;
-      uint_t             lreprobe_limit;
+      reprobe_limit_t    reprobe_limit;
       uint_t             key_len;  // original key len
       word               key_mask; // mask for high bits of hash(key)
       uint_t             key_off;  // offset in key field for reprobe value
@@ -83,12 +98,13 @@ namespace jellyfish {
 
       array(size_t _size, uint_t _key_len, uint_t _val_len,
             uint_t _reprobe_limit, size_t *_reprobes) :
-        lsize(ceilLog2(_size)), size(((size_t)1) << lsize), size_mask(size - 1),
-        reprobe_limit(_reprobe_limit), key_len(_key_len),
+        lsize(ceilLog2(_size)), size(((size_t)1) << lsize),
+        size_mask(size - 1),
+        reprobe_limit(_reprobe_limit, _reprobes, size), key_len(_key_len),
         key_mask(key_len <= lsize ? 0 : (((word)1) << (key_len - lsize)) - 1),
         key_off(key_len <= lsize ? 0 : key_len - lsize),
-        offsets(key_off + bitsize(_reprobe_limit + 1), _val_len,
-                _reprobe_limit + 1),
+        offsets(key_off + bitsize(reprobe_limit.val() + 1), _val_len,
+                reprobe_limit.val() + 1),
         mem_block(div_ceil(size, (size_t)offsets.get_block_len()) * offsets.get_block_word_len() * sizeof(word)),
         data((word *)mem_block.get_ptr()), reprobes(_reprobes),
         hash_matrix(key_len), 
@@ -146,11 +162,11 @@ namespace jellyfish {
             SquareBinaryMatrix &_hash_matrix, 
             SquareBinaryMatrix &_hash_inverse_matrix) :
         lsize(ceilLog2(_size)), size(_size), size_mask(size-1),
-        reprobe_limit(_reprobe_limit), key_len(_key_len),
+        reprobe_limit(_reprobe_limit, _reprobes, size), key_len(_key_len),
         key_mask(key_len <= lsize ? 0 : (((word)1) << (key_len - lsize)) - 1),
         key_off(key_len <= lsize ? 0 : key_len - lsize),
-        offsets(key_off + bitsize(_reprobe_limit + 1), _val_len,
-                _reprobe_limit + 1),
+        offsets(key_off + bitsize(reprobe_limit.val() + 1), _val_len,
+                reprobe_limit.val() + 1),
         data((word *)map), reprobes(_reprobes),
         hash_matrix(_hash_matrix), hash_inverse_matrix(_hash_inverse_matrix)
       { }
@@ -175,9 +191,9 @@ namespace jellyfish {
       uint_t get_key_len() const { return key_len; }
       uint_t get_val_len() const { return offsets.get_val_len(); }
       
-      uint_t get_max_reprobe() const { return reprobe_limit; }
+      uint_t get_max_reprobe() const { return reprobe_limit.val(); }
       size_t get_max_reprobe_offset() const { 
-        return reprobes[reprobe_limit]; 
+        return reprobes[reprobe_limit.val()]; 
       }
 
       
@@ -464,7 +480,7 @@ namespace jellyfish {
         // Resolve value
         reprobe = 0;
         cid = id = (id + reprobes[0]) & size_mask;
-        while(reprobe <= reprobe_limit) {
+        while(reprobe <= reprobe_limit.val()) {
           if(reprobe)
             cid  = (id + reprobes[reprobe]) & size_mask;
 
@@ -544,7 +560,7 @@ namespace jellyfish {
             if(nkey == akey)
               break;
           }
-          if(++reprobe > reprobe_limit)
+          if(++reprobe > reprobe_limit.val())
             return false;
           cid = (id + reprobes[reprobe]) & size_mask;
           akey = key | ((reprobe + 1) << key_off);
@@ -586,7 +602,7 @@ namespace jellyfish {
             }
 
             cid = (bid + reprobes[++reprobe]) & size_mask;
-          } while(reprobe <= reprobe_limit);
+          } while(reprobe <= reprobe_limit.val());
         }
 
         return true;
@@ -640,7 +656,7 @@ namespace jellyfish {
             key_claimed = set_key(kw, nkey, o->key.mask1, ao->key.mask1);
           }
           if(!key_claimed) { // reprobe
-            if(++reprobe > reprobe_limit)
+            if(++reprobe > reprobe_limit.val())
               return false;
             cid = (id + reprobes[reprobe]) & size_mask;
 
@@ -727,7 +743,7 @@ namespace jellyfish {
             key_claimed = set_key(kw, nkey, o->key.mask1, ao->key.mask1, is_new);
           }
           if(!key_claimed) { // reprobe
-            if(++reprobe > reprobe_limit)
+            if(++reprobe > reprobe_limit.val())
               return false;
             cid = (*id + reprobes[reprobe]) & size_mask;
 

commit ac34f15944c0540d787778e8cde3637dbbbfb19b

Fixed bug with large key in invertible_array, introduced in r1340.
git-svn-id: https://svn.cs.umd.edu/repos/carlksvn/jellyfish/branch-releases/version-1.1@1372 3840e3e0-9109-4a78-8b6e-c2447a23df0a

jellyfish/Makefile.am
@@ -15,7 +15,7 @@ query_cmdline.cc hash_merge_cmdline.cc histo_cmdline.cc			\
 stats_cmdline.cc histo_fastq_main_cmdline.cc				\
 dump_fastq_main_cmdline.cc mer_counter_cmdline.cc			\
 hash_fastq_merge_cmdline.cc dump_cmdline.cc dbg.cc mapped_file.cc	\
-backtrace.cc
+backtrace.cc floats.cc
 # jellyfish_CPPFLAGS = $(AM_CPPFLAGS)
 
 EXTRA_DIST = cite_cmdline.hpp query_cmdline.hpp hash_merge_cmdline.hpp	\
@@ -30,7 +30,7 @@ libjellyfish_la_SOURCES = square_binary_matrix.cc err.cc misc.cc	\
 dbg.hpp storage.cc thread_exec.cc time.cc file_parser.cc		\
 read_parser.cc parse_read.cc half.cpp mapped_file.cc parse_dna.cc	\
 parse_quake.cc parse_qual_dna.cc sequence_parser.cc seq_qual_parser.cc	\
-backtrace.cc
+backtrace.cc floats.cc
 libjellyfish_la_CPPFLAGS = $(AM_CPPFLAGS)
 library_includedir=$(includedir)/jellyfish-@PACKAGE_VERSION@/jellyfish
 library_include_HEADERS = allocators_malloc.hpp allocators_mmap.hpp	\

jellyfish/aligned_values_array.hpp
@@ -98,6 +98,17 @@ namespace jellyfish {
         return true;
       }
 
+      bool get_val(key_t key, val_t &val, bool full = true) const {
+        key_t  v_ignore;
+        size_t key_id;
+
+        if(!keys.get_val(key, key_id, v_ignore, false))
+          return false;
+
+        vals.get_val(key_id, val);
+        return true;
+      }
+
       class iterator {
         typename key_ary_t::iterator              key_it;
         const val_ary_t                    *const vals;

jellyfish/capped_integer.hpp
@@ -17,7 +17,13 @@
 #ifndef __JELLYFISH_CAPPED_INTEGER_HPP__
 #define __JELLYFISH_CAPPED_INTEGER_HPP__
 
+#include <iostream>
+
 namespace jellyfish {
+  template<typename T> class capped_integer;
+  template<typename T>
+  std::ostream &operator<<(std::ostream &os, const capped_integer<T> &i);
+
   template<typename T>
   class capped_integer {
     T x;
@@ -40,7 +46,15 @@ namespace jellyfish {
     float to_float() const { return (float)x; }
    
     bool operator==(const capped_integer &o) { return x == o.x; }
+    
+    friend std::ostream &operator<< <> (std::ostream &os, 
+                                        const capped_integer<T> &i);
   };
+
+  template<typename T>
+  std::ostream &operator<<(std::ostream &os, const capped_integer<T> &i) {
+    return os << i.x;
+  }
 }
 
 #endif

jellyfish/direct_indexing_array.hpp
@@ -74,6 +74,11 @@ namespace jellyfish {
         return true;
       }
 
+      bool get_val(key_t key, val_t &val, bool full = true) const {
+        val = data[key];
+        return true;
+      }
+
       class iterator {
         const array *ary;
         size_t       start_id;

jellyfish/floats.cc
@@ -1,0 +1,23 @@
+/*  This file is part of Jellyfish.
+
+    Jellyfish is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Jellyfish is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Jellyfish.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <jellyfish/floats.hpp>
+
+namespace jellyfish {
+  std::ostream &operator<<(std::ostream &os, const Float &f) {
+    return os << f.v.fv;
+  }
+}

jellyfish/floats.hpp
@@ -18,6 +18,7 @@
 #define __JELLYFISH_FLOATS_HPP__
 
 #include <stdint.h>
+#include <iostream>
 #ifdef HALF_FLOATS
 #include <jellyfish/half.h>
 #endif
@@ -44,6 +45,7 @@ namespace jellyfish {
 
   public:
     Float() : v(0.0f) {}
+    Float(int _v) : v((bits_t)_v) {}
     Float(float_t _v) : v(_v) {}
     Float(bits_t _v) : v(_v) {}
 
@@ -59,6 +61,7 @@ namespace jellyfish {
 
     // Should we use the floating point ==?
     bool operator==(Float o) { return v.iv == o.v.iv; }
+    friend std::ostream &operator<<(std::ostream &os, const Float &f);
   };
 }
 

jellyfish/invertible_hash_array.hpp
@@ -636,7 +636,8 @@ namespace jellyfish {
         uint64_t hash = hash_matrix.times(_key);
         word *w;
         *id = hash & size_mask;
-        return claim_key((hash >> lsize) & key_mask, is_new, id, &ao, &w);
+        return claim_key((hash >> lsize) & key_mask, is_new, id, false,
+                         &ao, &w);
       }
 
       void write_ary_header(std::ostream *out) const {
@@ -660,24 +661,26 @@ namespace jellyfish {
        * is_new is set on output to true if key did not exists in hash
        * before. *ao points to the actual offsets object.
        */
-      bool claim_key(const word &key, bool *is_new, size_t *id, 
+      bool claim_key(const word &key, bool *is_new, size_t *id, bool large,
                      const offset_t **_ao, word **_w) {
         uint_t		 reprobe     = 0;
         const offset_t	*o, *lo, *ao;
         word		*w, *kw, nkey;
         bool		 key_claimed = false;
         size_t		 cid         = *id;
-        word		 akey        = key | ((word)1 << key_off);
+        word		 akey        = large ? 0 :(key | ((word)1 << key_off));
 
         do {
           *_w  = w = offsets.get_word_offset(cid, &o, &lo, data);
-          *_ao = ao = o;
+          *_ao = ao = large ? lo : o;
 
           kw = w + ao->key.woff;
 
           if(ao->key.mask2) { // key split on two words
             nkey = akey << ao->key.boff;
             nkey |= ao->key.sb_mask1;
+            if(large)
+              nkey |= ao->key.lb_mask;
             nkey &= ao->key.mask1;
 
             // Use o->key.mask1 and not ao->key.mask1 as the first one is
@@ -690,6 +693,8 @@ namespace jellyfish {
             }
           } else { // key on one word
             nkey = akey << ao->key.boff;
+            if(large)
+              nkey |= ao->key.lb_mask;
             nkey &= ao->key.mask1;
             key_claimed = set_key(kw, nkey, o->key.mask1, ao->key.mask1, is_new);
           }
@@ -698,7 +703,10 @@ namespace jellyfish {
               return false;
             cid = (*id + reprobes[reprobe]) & size_mask;
 
-            akey = key | ((reprobe + 1) << key_off);
+            if(large)
+              akey = reprobe;
+            else
+              akey = key | ((reprobe + 1) << key_off);
           }
         } while(!key_claimed);
 
@@ -711,7 +719,7 @@ namespace jellyfish {
         word		*w;
 
         bool is_new;
-        if(!claim_key(key, &is_new, &id, &ao, &w))
+        if(!claim_key(key, &is_new, &id, large, &ao, &w))
           return false;
         if(oval)
           *oval = !is_new;

jellyfish/sorted_dumper.hpp
@@ -100,7 +100,6 @@ namespace jellyfish {
   void sorted_dumper<storage_t,atomic_t>::_dump() {
     std::ofstream _out;
     assert(dump_mutex.try_lock());
-    DBG << V(file_prefix) << V(file_index);
     open_next_file(file_prefix.c_str(), &file_index, _out);
     out = &_out;
     unique = distinct = total = max_count = 0;

tests/big.sh
@@ -8,17 +8,23 @@ fi
 . ./compat.sh
 
 sort > ${pref}.md5sum <<EOF 
-019a57f22208f638f58b68a3131c4692 ${pref}.histo
+019a57f22208f638f58b68a3131c4692 ${pref}_16.histo
 EOF
+
+    # $JF count -m 31 -s 4000000000 -o ${pref}_31 -c 4 -p 253 -C -r -t $nCPUs --timing ${pref}_31.timing \
+    # seq30g.fa && \
+    # $JF histo ${pref}_31_0 > ${pref}_31.histo && \
+
 
 echo "Generate 30 Gigs of sequence and count k-mers on it" && \
     ([ -f seq30g.fa ] || ../jellyfish/generate_sequence -v -o seq30g -r 1000 -s 1602176487 30000000000) && \
-    $JF count -m 16 -s 4000000000 -o ${pref} -c 4 -p 254 -C --out-counter-len 2 \
-    -t $nCPUs --timing ${pref}.timing seq30g.fa && \
-    $JF histo ${pref}_0 > ${pref}.histo && \
+    $JF count -m 16 -s 4000000000 -o ${pref}_16 -c 4 -p 253 -C --out-counter-len 2 \
+    -t $nCPUs --timing ${pref}_16.timing seq30g.fa && \
+    $JF histo ${pref}_16_0 > ${pref}_16.histo && \
     check ${pref}.md5sum
 RET=$?
 
-cat ${pref}.timing
+echo "16-mer timing"; cat ${pref}_16.timing
+#echo "31-mer timing"; cat ${pref}_31.timing
 
 exit $RET