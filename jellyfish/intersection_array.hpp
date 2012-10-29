8d02f72251810f5953faf1b7f487ee9f0c099fc8
Guillaume Marcais <Guillaume Marcais guillaume@marcais.net>
2012-10-29 16:20:07 -0400

    Implemented size doubling

--- jellyfish/intersection_array.hpp
+++ jellyfish/intersection_array.hpp
@@ -16,8 +16,8 @@ class intersection_array {
 
   array*                  ary_;
   array*                  new_ary_;
-  unsigned char*                   mer_status_;
-  unsigned char*                   new_mer_status_;
+  unsigned char*          mer_status_;
+  unsigned char*          new_mer_status_;
   uint16_t                nb_threads_;
   locks::pthread::barrier size_barrier_;
   volatile uint16_t       size_thid_, done_threads_;
@@ -78,7 +78,7 @@ public:
     while(!double_size()) ;
   }
 
-  void add(const mer_dna& k) {
+  void add(const Key& k) {
     bool   is_new;
     size_t id;
     while(!ary_->set(k, &is_new, &id))
@@ -131,23 +131,86 @@ public:
   }
 
   mer_info operator[](const Key& k) const {
-    mer_info res;
     size_t   id;
 
-    res.raw = 0;
-    if(!ary_->get_key_id(k, &id))
+    if(!ary_->get_key_id(k, &id)) {
+      mer_info res;
+      res.raw = 0;
       return res;
+    }
+    return info_at(id);
+  }
 
-    unsigned char raw = mer_status_[id / 2];
+
+protected:
+  mer_info info_at(size_t id) const {
+    mer_info res;
+    res.raw = mer_status_[id / 2];
     if(id & 0x1)
-      raw >>= 4;
-    res.raw = raw;
+      res.raw >>= 4;
 
     return res;
   }
 
-protected:
-  bool double_size() { }
+  void add_new(const Key& k, mer_info i) {
+    bool   is_new;
+    size_t id;
+    new_ary_->set(k, &is_new, &id);
+
+    unsigned char oinfo;
+    unsigned char ninfo = new_mer_status_[id / 2];
+    do {
+      oinfo = ninfo;
+      if(id & 0x1)
+        ninfo = (ninfo & 0xf) | (i.raw << 4);
+      else
+        ninfo = (ninfo & 0xf0) | (i.raw & 0xf);
+      ninfo = atomic::gcc::cas(&new_mer_status_[id / 2], oinfo, ninfo);
+    } while(oinfo != ninfo);
+
+  }
+
+  bool double_size() {
+    if(size_barrier_.wait()) {
+      // Serial thread -> allocate new array for size doubling
+      if(done_threads_ < nb_threads_) {
+        new_ary_        = new array(ary_->size() * 2, ary_->key_len(), 0, 126, jellyfish::quadratic_reprobes);
+        new_mer_status_ = new unsigned char[ary_->size()];
+        memset(new_mer_status_, '\0', ary_->size());
+        size_thid_      = 0;
+      } else {
+        new_ary_        = 0;
+        new_mer_status_ = 0;
+        done_threads_   = 0;
+      }
+    }
+
+    // Return if all done
+    size_barrier_.wait();
+    array* my_ary = *(array* volatile*)&new_ary_;
+    if(my_ary == 0)
+      return true;
+
+    // Copy data from old to new
+    uint16_t                 id = atomic::gcc::fetch_add(&size_thid_, (uint16_t)1);
+    typename array::iterator it = ary_->iterator_slice(id, nb_threads_);
+    while(it.next()) {
+      assert(it.id() >= 0 && it.id() < ary_->size());
+      add_new(it.key(), info_at(it.id()));
+    }
+
+    if(size_barrier_.wait()) {
+      // Serial thread -> set new ary to be current
+      delete ary_;
+      delete [] mer_status_;
+      ary_        = new_ary_;
+      mer_status_ = new_mer_status_;
+    }
+
+    // Done. Last sync point
+    size_barrier_.wait();
+    return false;
+  }
 };
 }
 
--- unit_tests/test_intersection.cc
+++ unit_tests/test_intersection.cc
@@ -9,13 +9,7 @@ using jellyfish::mer_dna;
 typedef jellyfish::intersection_array<mer_dna> inter_array;
 typedef std::set<mer_dna> mer_set;
 
-TEST(IntersectionArray, Mers) {
-  mer_dna::k(20);
-  inter_array ary(1000, mer_dna::k() * 2, 1);
-  static const int nb_mers = 100;
-  mer_set all, mult, uniq;
-
-  // Fill sets
+void fill_sets(const int nb_mers, mer_set& all, mer_set& mult, mer_set& uniq) {
   mer_dna m;
   for(int i = 0; i < nb_mers; ++i) {
     m.randomize();
@@ -25,52 +19,63 @@ TEST(IntersectionArray, Mers) {
     m.randomize();
     uniq.insert(m);
   }
+}
 
-  // Insert into ary as if 3 files
-
-  {
-    // First file. Contains the first third of uniq mers and the first
-    // two thirds of the multiple mers.
-    mer_set::const_iterator uit = uniq.begin();
-    mer_set::const_iterator mit = mult.begin();
-    for(mer_set::const_iterator it = all.begin(); it != all.end(); ++it)
-      ary.add(*it);
-    for(int i = 0; i < nb_mers / 3; ++i, ++uit, ++mit) {
-      ary.add(*uit);
-      ary.add(*mit);
-    }
-    mer_set::const_iterator mit1third = mit;
-    for(int i = 0; i < nb_mers / 3; ++i, ++mit)
-      ary.add(*mit);
-    ary.postprocess(0, true);
-
-    // Second file. Contains the second third of uniq mers and the last
-    // two thirds of the multiple mers.
-    for(mer_set::const_iterator it = all.begin(); it != all.end(); ++it)
-      ary.add(*it);
-    for(int i = 0; i < nb_mers / 3; ++i, ++uit)
-      ary.add(*uit);
-    for(mer_set::const_iterator it = mit1third; it != mult.end(); ++it)
-      ary.add(*it);
-    ary.postprocess(0, false);
-
-    // Third file. Contains the last third of uniq mers, and the first
-    // and last thirds of the multiple mers.
-    for(mer_set::const_iterator it = all.begin(); it != all.end(); ++it)
-      ary.add(*it);
-    for( ; uit != uniq.end(); ++uit)
-      ary.add(*uit);
-    mer_set::const_iterator nmit = mult.begin();
-    for(int i = 0; i < nb_mers / 3; ++i, ++nmit)
-      ary.add(*nmit);
-    for( ; mit != mult.end(); ++mit)
-      ary.add(*mit);
-    ary.postprocess(0, false);
+void insert_mers(inter_array& ary, const int nb_mers, const mer_set& all, const mer_set& mult, const mer_set& uniq) {
+  // First file. Contains the first third of uniq mers and the first
+  // two thirds of the multiple mers.
+  mer_set::const_iterator uit = uniq.begin();
+  mer_set::const_iterator mit = mult.begin();
+  for(mer_set::const_iterator it = all.begin(); it != all.end(); ++it)
+    ary.add(*it);
+  for(int i = 0; i < nb_mers / 3; ++i, ++uit, ++mit) {
+    ary.add(*uit);
+    ary.add(*mit);
   }
+  mer_set::const_iterator mit1third = mit;
+  for(int i = 0; i < nb_mers / 3; ++i, ++mit)
+    ary.add(*mit);
+  ary.postprocess(0, true);
+
+  // Second file. Contains the second third of uniq mers and the last
+  // two thirds of the multiple mers.
+  for(mer_set::const_iterator it = all.begin(); it != all.end(); ++it)
+    ary.add(*it);
+  for(int i = 0; i < nb_mers / 3; ++i, ++uit)
+    ary.add(*uit);
+  for(mer_set::const_iterator it = mit1third; it != mult.end(); ++it)
+    ary.add(*it);
+  ary.postprocess(0, false);
+
+  // Third file. Contains the last third of uniq mers, and the first
+  // and last thirds of the multiple mers.
+  for(mer_set::const_iterator it = all.begin(); it != all.end(); ++it)
+    ary.add(*it);
+  for( ; uit != uniq.end(); ++uit)
+    ary.add(*uit);
+  mer_set::const_iterator nmit = mult.begin();
+  for(int i = 0; i < nb_mers / 3; ++i, ++nmit)
+    ary.add(*nmit);
+  for( ; mit != mult.end(); ++mit)
+    ary.add(*mit);
+  ary.postprocess(0, false);
+}
+
+TEST(IntersectionArray, Mers) {
+  mer_dna::k(20);
+  inter_array ary(1000, mer_dna::k() * 2, 1);
+  static const int nb_mers = 100;
+  mer_set all, mult, uniq;
+
+  fill_sets(nb_mers, all, mult, uniq);
+  insert_mers(ary, nb_mers, all, mult, uniq);
+  ary.done();
 
   for(mer_set::const_iterator it = all.begin(); it != all.end(); ++it) {
     inter_array::mer_info info = ary[*it];
     EXPECT_TRUE(info.info.all);
+    EXPECT_TRUE(info.info.mult);
+    EXPECT_TRUE(info.info.uniq);
   }
 
   for(mer_set::const_iterator it = mult.begin(); it != mult.end(); ++it) {
@@ -86,7 +91,21 @@ TEST(IntersectionArray, Mers) {
     EXPECT_FALSE(info.info.mult);
     EXPECT_FALSE(info.info.all);
   }
+}
 
+TEST(IntersectionArray, Doubling) {
+  mer_dna::k(20);
+  inter_array ary_large(1000, mer_dna::k() * 2, 1);
+  inter_array ary_small(100, mer_dna::k() * 2, 1);
+
+  static const int nb_mers = 100;
+  mer_set all, mult, uniq;
+
+  fill_sets(nb_mers, all, mult, uniq);
+  insert_mers(ary_large, nb_mers, all, mult, uniq);
+  ary_large.done();
+  insert_mers(ary_small, nb_mers, all, mult, uniq);
+  ary_small.done();
 }
 
 } // namespace {
