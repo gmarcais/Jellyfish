e4a570138972ba1a33351cf6f10aea90c882e09b
Guillaume Marcais <Guillaume Marcais guillaume@marcais.net>
2012-10-31 13:33:51 -0400

    Fixed distcheck.

--- tests/test_intersection.sh
+++ tests/test_intersection.sh
@@ -16,7 +16,7 @@ $GS -o intersection_mult -s 3348383312 -r $RLEN $MULT
 cat intersection_all.fa intersection_mult.fa intersection_uniq.fa > uniq.fa
 cat intersection_mult.fa intersection_all.fa > mult.fa
 
-time $JF -s 2M -m $MLEN -t 1 uniq.fa mult.fa intersection_all.fa
+time $JF intersection -s 2M -m $MLEN -t 1 uniq.fa mult.fa intersection_all.fa
 
 test "$((($RLEN - $MLEN + 1) * ($ALL / $RLEN)))" = "$(wc -l < intersection)"
 test "$((($RLEN - $MLEN + 1) * ($UNIQ / $RLEN)))" = "$(wc -l < uniq_uniq.fa)"
