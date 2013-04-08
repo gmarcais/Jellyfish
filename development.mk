d5ec56b78408d5d6b69d115ecf18942ee72a7d04
Guillaume Marcais <Guillaume Marcais guillaume@marcais.net>
2013-04-08 12:27:30 -0400

    Fixed README and some help strings.

--- development.mk
+++ development.mk
@@ -1,1 +1,13 @@
 # AM_CPPFLAGS += -Werror
+
+# Count lines of code
+.PHONY: cloc
+cloc:
+	cloc --force-lang="Ruby,yaggo" --force-lang="make,am" --force-lang="make,mk" \
+	  --exclude-dir="gtest" --ignored=cloc_ignored_src_files \
+	  $(top_srcdir)/jellyfish $(top_srcdir)/include $(top_srcdir)/lib $(top_srcdir)/sub_commands $(top_srcdir)/tests $(top_srcdir)/unittests \
+	  $(top_srcdir)/Makefile.am $(top_srcdir)/*.mk
+
+
+# Make a dependency on yaggo the software
+$(YAGGO_SOURCES): $(YAGGO)
