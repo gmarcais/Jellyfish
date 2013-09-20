17c6611b3a91402a6f37c8586ea314d5d094aab3
Guillaume Marcais <Guillaume Marcais guillaume@marcais.net>
2013-09-20 15:10:06 -0400

    More cloc targets

--- development.mk
+++ development.mk
@@ -1,13 +1,18 @@
 AM_CXXFLAGS += -Werror
 
 # Count lines of code
-.PHONY: cloc
+.PHONY: cloc cloc_jellyfish
 cloc:
 	cloc --force-lang="Ruby,yaggo" --force-lang="make,am" --force-lang="make,mk" \
 	  --exclude-dir="gtest" --ignored=cloc_ignored_src_files \
-	  $(top_srcdir)/jellyfish $(top_srcdir)/include $(top_srcdir)/lib $(top_srcdir)/sub_commands $(top_srcdir)/tests $(top_srcdir)/unittests \
+	  $(top_srcdir)/jellyfish $(top_srcdir)/include $(top_srcdir)/lib $(top_srcdir)/sub_commands $(top_srcdir)/tests $(top_srcdir)/unit_tests \
 	  $(top_srcdir)/Makefile.am $(top_srcdir)/*.mk
 
+cloc_jellyfish:
+	cloc $(top_srcdir)/jellyfish $(top_srcdir)/include $(top_srcdir)/lib $(top_srcdir)/sub_commands
+
+cloc_library:
+	cloc $(top_srcdir)/include $(top_srcdir)/lib
 
 # Make a dependency on yaggo the software
 $(YAGGO_SOURCES): $(YAGGO)
