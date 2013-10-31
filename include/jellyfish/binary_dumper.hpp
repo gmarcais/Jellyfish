25c22b4766920e4b4af3f70ee903ecf96ef96f5e
Guillaume Marcais <Guillaume Marcais guillaume@marcais.net>
2013-10-31 14:30:37 -0400

    More improvement in val_id to query hash on disk.

--- include/jellyfish/binary_dumper.hpp
+++ include/jellyfish/binary_dumper.hpp
@@ -169,7 +169,7 @@ public:
     if(pos < first_pos_ || pos > last_pos_)
       return false;
     uint64_t first = 0, last = last_id_;
-    while(first < last - 1) {
+    while(first + 1 < last) {
       *id = (first + last) / 2;
       key_at(*id, mid_key_);
       if(key == mid_key_) {
