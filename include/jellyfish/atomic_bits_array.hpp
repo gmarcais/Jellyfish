8b4d86158ed630c5b67c49ca436e4e056cda8840
Guillaume Marcais <Guillaume Marcais guillaume@marcais.net>
2014-05-28 10:05:22 -0400

    atomic_bits_array: set method returns false only if the value has changed since last get, not if bits in the same word have changed.

--- include/jellyfish/atomic_bits_array.hpp
+++ include/jellyfish/atomic_bits_array.hpp
@@ -91,12 +91,18 @@ class atomic_bits_array_base {
     }
 
     bool set(Value& nval) {
-      const T new_word    = (prev_word_ & ~mask_) | ((static_cast<T>(nval) << off_) & mask_);
-      const T actual_word = atomic_.cas(word_, prev_word_, new_word);
-      if(__builtin_expect(actual_word == prev_word_, 1))
-        return true;
-      prev_word_ = actual_word;
-      nval       = get_val(prev_word_);
+      Value pval;
+      Value cval = get_val(prev_word_);
+      do {
+        pval = cval;
+        const T new_word    = (prev_word_ & ~mask_) | ((static_cast<T>(nval) << off_) & mask_);
+        const T actual_word = atomic_.cas(word_, prev_word_, new_word);
+        if(__builtin_expect(actual_word == prev_word_, 1))
+          return true;
+        prev_word_ = actual_word;
+        cval = get_val(prev_word_);
+      } while(pval == cval);
+      nval = cval;
       return false;
     }
   };
