56edde696a87da6bc340a0ee0c73b94a53674db9
Guillaume Marcais <Guillaume Marcais guillaume@marcais.net>
2018-09-17 14:20:05 -0400

    Changed to dual license BSD3-GPL3

--- include/jellyfish/whole_sequence_parser.hpp
+++ include/jellyfish/whole_sequence_parser.hpp
@@ -1,4 +1,11 @@
 #ifndef __JELLYFISH_WHOLE_SEQUENCE_PARSER_HPP__
+/*  This file is part of Jellyfish.
+
+    This work is dual-licensed under 3-Clause BSD License or GPL 3.0.
+    You can choose between one of them if you use this work.
+
+`SPDX-License-Identifier: BSD-3-Clause OR  GPL-3.0`
+*/
 #define __JELLYFISH_WHOLE_SEQUENCE_PARSER_HPP__
 
 #include <string>
