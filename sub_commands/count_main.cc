0c388e8d9645f5368e02ed9ee5550ca320b840b7
pkubaj <pkubaj@users.noreply.github.com>
2019-05-13 09:57:49 +0200

    Fix build with GCC8

--- sub_commands/count_main.cc
+++ sub_commands/count_main.cc
@@ -360,7 +360,7 @@ int count_main(int argc, char *argv[])
         uint64_t max = args.upper_count_given ? args.upper_count_arg : std::numeric_limits<uint64_t>::max();
         try {
           merge_files(files, args.output_arg, header, min, max);
-        } catch(MergeError e) {
+        } catch(MergeError &e) {
           err::die(err::msg() << e.what());
         }
         if(!args.no_unlink_flag) {
--- sub_commands/merge_main.cc
+++ sub_commands/merge_main.cc
@@ -33,7 +33,7 @@ int merge_main(int argc, char *argv[])
 
   try {
     merge_files(args.input_arg, args.output_arg, out_header, min, max);
-  } catch(MergeError e) {
+  } catch(MergeError &e) {
     err::die(err::msg() << e.what());
   }
 
