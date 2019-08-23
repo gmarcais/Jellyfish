e4ad80f0597caa2758764109f07e5293440e6f5b
Guillaume Marcais <gmarcais@cs.cmu.edu>
2019-08-23 13:46:21 -0400

    Fix tests/small_mers.sh
    * Don't use bashisms. Use file system instead of <() construct.

--- tests/small_mers.sh
+++ tests/small_mers.sh
@@ -6,5 +6,7 @@ mkdir -p tests-data; cd tests-data
 for i in $(seq 2 10); do
     $JF count -t $nCPUs -o ${pref}_m${i}_10M_25.jf -s 10M -c 25 -m $i -C seq10m.fa
     $JF count -t $nCPUs -o ${pref}_m${i}_10M_5.jf -s 1k -c 5 -m $i -C seq10m.fa
-    diff -q <($JF histo ${pref}_m${i}_10M_25.jf) <($JF histo ${pref}_m${i}_10M_5.jf)
+    $JF histo ${pref}_m${i}_10M_25.jf > ${pref}_m${i}_10M_25.histo
+    $JF histo ${pref}_m${i}_10M_5.jf > ${pref}_m${i}_10M_5.histo
+    diff -q ${pref}_m${i}_10M_25.histo ${pref}_m${i}_10M_5.histo
 done
