7010e56382feb38b6ebccda10a6be6e779032bb7
Guillaume Marcais <gmarcais@cs.cmu.edu>
2021-09-10 10:39:13 -0400

    Keep up with autoconf and g++
    * Ran `autoupdate` to update obsolete constructs
    
    * Fix in jsconcpp.cpp to avoid a g++ warning.

--- lib/jsoncpp.cpp
+++ lib/jsoncpp.cpp
@@ -1733,7 +1733,7 @@ Value::CZString::operator<( const CZString &other ) const
 bool 
 Value::CZString::operator==( const CZString &other ) const 
 {
-   if ( cstr_ )
+   if ( cstr_ && other.cstr_ )
       return strcmp( cstr_, other.cstr_ ) == 0;
    return index_ == other.index_;
 }
